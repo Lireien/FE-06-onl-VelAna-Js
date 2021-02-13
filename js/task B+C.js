34a99ff80efbb8a98d7201adc1087653e21c7bc4
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-02-13 13:21:43 +0300

    Task-B

---
+++ js/task B+C.js
@@ -0,0 +1,21 @@
+// Задание B!
+
+// Создадим шесть переменных, в каждой из которых будет один из шести типов данных;
+// Выводим их значение в alert;
+// Выводим объект в консоль.
+
+var numeric = 1;
+var strings = 'string';
+var booleans = 5>2;
+var undefined;
+var nothing = null;
+var object = { 
+  name : 'Peter', 
+  alias : 'SpiderMan' };
+
+alert(numeric);
+alert(strings);
+alert(booleans);
+alert(undefined);
+alert(nothing);
+alert(object);
