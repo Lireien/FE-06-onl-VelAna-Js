3f0abb8cfc5fb0dbd3e7c3562cc33a79d0a903d0
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-02-27 14:21:06 +0300

    part of constructor

---
+++ js/constructor.js
@@ -0,0 +1,27 @@
+// Задание 3! Функция-конструктор.
+// ● Напишем функцию-конструктор, которая принимает массив, и содержит три метода:
+// 1. возвращает количество элементов этого массива.
+// 2. выводит новый массив, содержащий количество символов каждой строки.
+// Прим на входе [“пирамида”, “юг”, “параллелограмм”] на выходе [8, 2, 14]
+// 3. выводит их в виде строки, разделенной запятыми.
+
+function iAmConstructor (arr){
+this.data = arr;
+this.getElementsQuantity = function(){
+  return this.data.length;
+}
+
+
+
+
+this.getStringWithComma = function(){
+  return this.data.join(', ')
+}
+}
+
+
+
+
+const words = new iAmConstructor(['пирамида', 'юг', 'параллелограмм']);
+console.log(words.getElementsQuantity());
+console.log(words.getStringWithComma())
