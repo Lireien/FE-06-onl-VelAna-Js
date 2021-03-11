92f3866878a515d91ac9e57db6b634bab035835b
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-03-11 12:32:14 +0300

    some corrections after new lection

--- js/modal.js
+++ js/modal.js
@@ -6,13 +6,34 @@ document.querySelector('.btn.add-user').addEventListener('click', function () {
   modalWindow.classList.remove('hidden');
 });
 
-document.querySelector('.close').addEventListener('click', function () {
-  modalWindow.classList.add('hidden');
-  document.getElementById('popap-form').reset();
-});
+document.querySelector('.close').addEventListener('click', clearForm);
 
-document.querySelector('.btn.close').addEventListener('click', function () {
-  modalWindow.classList.add('hidden');
-  document.getElementById('popap-form').reset();
+document.querySelector('.btn.close').addEventListener('click',clearForm);
+  function clearForm (){
+    modalWindow.classList.add('hidden');
+    document.getElementById('popap-form').reset();
 
-});
+};
+
+
+// Дополнительное задание!
+// ● Расширим функционал модального окна из занятия #7.
+// ● При добавлении пользователя через модальное окно, добавим полученный объект в массив пользователей.
+// ● Снова отрисуем массив, уже с новым пользователем.
+const okButtonElement = document.querySelector('.ok')
+const inputElements = document.querySelectorAll('.popap-form input');
+okButtonElement.addEventListener('click', addNewUser);
+const users = [
+  {firstName:'',lastName: '',age:''}
+]
+function addNewUser(){
+  const newUser = [];
+  inputElements.forEach(inputElement => {
+    newUser[inputElement.name] = inputElement.value;
+  });
+  users.push(newUser);
+  console.log(newUser);
+  clearForm();
+} 
+  
+  
---
+++ js/usersDataArr.js
@@ -0,0 +1,11 @@
+// Самостоятельное задание!
+// ● Создадим массив объектов, который будет содержать данные о пользователях;
+// ● В HTML документе создадим пустую таблицу, с заголовками
+// firstName lastName и age;
+// ● Заполним таблицу данными, приходящими из массива.
+
+let data = [
+{firstName: 'Ashton',lastName: 'Kutcher',age: 40,},
+{firstName: 'Bradley',lastName: 'Pitt',age: 54,},
+{firstName: 'Hannah',lastName: 'Dakota',age: 24,},
+];
