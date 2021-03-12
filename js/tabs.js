aeeb4216b0371baa8e208847566f1a0a1bf8d5e1
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-03-12 22:15:13 +0300

    three tabs with content

---
+++ js/tabs.js
@@ -0,0 +1,24 @@
+let switchTitleElements = document.querySelectorAll('.tabs-header__item');
+let switchContentElements = document.querySelectorAll('.tabs-content__item');
+let closeButtonElements = document.querySelectorAll('.close');
+
+switchTitleElements.forEach(function (item) {
+  item.addEventListener('click', function () {
+    let tabName = this.dataset.tab;
+    // console.log(tabName);
+    let tabContent = document.querySelector(
+      '.tabs-content__item[data-tab = "' + tabName + '"]'
+    );
+
+    switchContentElements.forEach(function (item) {
+      if (item.classList === 'active') {
+      }
+      item.classList.remove('active');
+    });
+    switchTitleElements.forEach(function (item) {
+      item.classList.remove('active');
+    });
+    tabContent.classList.add('active');
+    this.classList.toggle('active');
+  });
+});
