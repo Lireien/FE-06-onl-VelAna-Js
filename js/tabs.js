08508f5828d4914c611356d24c244c5fad7f808c
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-03-13 13:08:16 +0300

    still without deleting (

--- js/tabs.js
+++ js/tabs.js
@@ -2,6 +2,7 @@ let switchTitleElements = document.querySelectorAll('.tabs-header__item');
 let switchContentElements = document.querySelectorAll('.tabs-content__item');
 let closeButtonElements = document.querySelectorAll('.close');
 
+
 switchTitleElements.forEach(function (item) {
   item.addEventListener('click', function () {
     let tabName = this.dataset.tab;
@@ -9,13 +10,15 @@ switchTitleElements.forEach(function (item) {
     let tabContent = document.querySelector(
       '.tabs-content__item[data-tab = "' + tabName + '"]'
     );
-
+      
     switchContentElements.forEach(function (item) {
       if (item.classList === 'active') {
       }
       item.classList.remove('active');
     });
     switchTitleElements.forEach(function (item) {
+      if (item.classList === 'active') {
+      }
       item.classList.remove('active');
     });
     tabContent.classList.add('active');
