e295b8c3fb8d9610bab6b9ff311610d2cdc4708b
vel_ana <76497435+Lireien@users.noreply.github.com>
2021-03-16 14:35:10 +0300

    adding remove function after lesson

--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,15 @@
 let switchTitleElements = document.querySelectorAll('.tabs-header__item');
-let switchContentElements = document.querySelectorAll('.tabs-content__item');
+let switchContentElements = document.querySelectorAll('.content');
 let closeButtonElements = document.querySelectorAll('.close');
-
 
 switchTitleElements.forEach(function (item) {
   item.addEventListener('click', function () {
     let tabName = this.dataset.tab;
     // console.log(tabName);
     let tabContent = document.querySelector(
-      '.tabs-content__item[data-tab = "' + tabName + '"]'
+      '.content[data-tab = "' + tabName + '"]'
     );
-      
+
     switchContentElements.forEach(function (item) {
       if (item.classList === 'active') {
       }
@@ -25,3 +24,43 @@ switchTitleElements.forEach(function (item) {
     this.classList.toggle('active');
   });
 });
+
+closeButtonElements.forEach((closeButtonElements) => {
+  closeButtonElements.addEventListener('click', closeTabHandler);
+});
+
+function closeTabHandler(evt) {
+  evt.stopPropagation();
+  // 1stvar
+  // const allTabsElements = document.querySelectorAll('.tabs-header__item');
+  // if (allTabsElements.length < 2) {
+  //   return;
+  // }
+
+  let switchTitleElement = evt.target.parentElement;
+  const contentTab = switchTitleElement.dataset.tab;
+  // console.log(contentTab);
+  let tabName = switchTitleElement.dataset.tab;
+  // console.log(tabName);
+  const contentElement = document.querySelector(
+    '.content[data-tab = "' + tabName + '"]'
+  );
+  contentElement.remove();
+  switchTitleElement.remove();
+
+  const tabActiveElement = document.querySelector('.tabs-header__item .active');
+  if (!tabActiveElement) {
+    const tabElement = document.querySelector('.tabs-header__item');
+    tabElement.classList.add('active');
+    let tabDataTab = tabElement.dataset.tab;
+    const tabContentElement = document.querySelector(
+      '.content[data-tab = "' + tabDataTab + '"]'
+    );
+    tabContentElement.classList.add('active');
+  }
+  // 2ndvar
+  const allTabsElements = document.querySelectorAll('.tabs-header__item');
+  if(allTabsElements.length===1){
+    document.querySelector(`.tabs-header__item .close`).remove();
+  }
+}
